427cb60688ddec6af50b1da866dc7c7a6e45c5f5
sacidpak <mhscpak@gmail.com>
2019-06-13 19:20:11 +0300

    code review

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,29 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow mb-5">
     <router-link
-    to="/"
-    tag="a"
-    class="navbar-brand"
-    >Home</router-link>
+      to="/"
+      tag="a"
+      class="navbar-brand">
+          Home
+      </router-link>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <router-link
-        tag="li"
-        class="nav-item"
-        to="/about"
-        >
-        <a class="nav-link">About</a>
+          tag="li"
+          class="nav-item"
+          to="/about">
+            <a class="nav-link">About</a>
         </router-link>
         <router-link
-        tag="li"
-        class="nav-item"
-        to="/auth"
-        >
-        <a class="nav-link">Sign In</a>
+          tag="li"
+          class="nav-item"
+          to="/auth">
+            <a class="nav-link">Sign In</a>
         </router-link>
       </ul>
       <ul class="navbar-nav my-2 my-lg-0" :class="logoutClass">
         <li class="nav-item">
-          <a @click.prevent="logout" class="nav-link" href="#">Çıkış Yap</a>
+          <a @click.prevent="logout" class="nav-link" href="#">Sing Out</a>
         </li>
       </ul>
     </div>
